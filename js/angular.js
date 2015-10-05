629be27ab9a3d25415da4a4527f915a6917fb724
bbouley <b.bouley@hotmail.co.uk>
2015-10-04 20:06:24 -0600

    pushing up css and photo changes

--- js/angular.js
+++ js/angular.js
@@ -1,7 +1,10 @@
-// sample angular code
+// samplepro angular code
 
 var app = angular.module('myApp', []);
 
 app.controller('myController', function($scope) {
   $scope.greeting = "Hello World!";
+  $scope.showTitle = true;
+  $scope.projects = projects;
+  $scope.apps = apps;
 });
---
+++ js/projects.js
@@ -0,0 +1,17 @@
+var projects = [
+{
+  title: 'Testing',
+  technologies: 'This part is testing showing up the technologies that I used in the projects',
+  description: 'This is a test of the description I want to have regarding my projects',
+  image : 'linear-gradient(to left, hsla(0, 0%, 0%, 0.7), hsla(0, 0%, 0%, 0.8)), url("http://www.designzzz.com/wp-content/uploads/2011/06/css-colors.jpg")'
+}
+];
+
+var apps = [
+{
+  title: 'Testing',
+  technologies: 'This part is testing showing up the technologies that I used in the projects',
+  description: 'This is a test of the description I want to have regarding my projects',
+  image : 'linear-gradient(to left, hsla(0, 0%, 0%, 0.4), hsla(0, 0%, 0%, 0.7)), url("images/doodle-1.jpg")'
+}
+];
